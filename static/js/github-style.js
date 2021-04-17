9f32748eeb148fad1138e7dc8fe3e81c2796b813
Alberto Rosado <arosado@hey.com>
2021-04-17 15:25:41 -0700

    add netlify cms crap

--- static/js/github-style.js
+++ static/js/github-style.js
@@ -23,6 +23,16 @@ let contributions;
 
   yearList();
   switchYear(year.toString());
+
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  }
 })();
 
 function switchYear(year) {
--- static/js/theme-mode.js
+++ static/js/theme-mode.js
@@ -1,38 +1,41 @@
 function switchTheme() {
   const currentTheme = getCurrentTheme();
-  if (currentTheme == 'light') {
-    setTheme('dark');
+  if (currentTheme == "light") {
+    setTheme("dark");
   } else {
-    setTheme('light');
+    setTheme("light");
   }
 }
 
 function setTheme(style) {
-  document.querySelectorAll('.isInitialToggle').forEach(elem => {
-    elem.classList.remove('isInitialToggle');
+  document.querySelectorAll(".isInitialToggle").forEach((elem) => {
+    elem.classList.remove("isInitialToggle");
   });
-  document.documentElement.setAttribute('data-color-mode', style);
-  localStorage.setItem('data-color-mode', style);
+  document.documentElement.setAttribute("data-color-mode", style);
+  localStorage.setItem("data-color-mode", style);
 }
 
 function getCurrentTheme() {
-  const localStyle = localStorage.getItem('data-color-mode');
-  const systemStyle = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+  const localStyle = localStorage.getItem("data-color-mode");
+  const systemStyle =
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
   return localStyle || systemStyle;
 }
 
 function detectTheme() {
-  if(window.matchMedia) {
+  if (window.matchMedia) {
     setTheme(getCurrentTheme());
-
     // listen for theme changes
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener(e => {
-      if(e.matches) {
-        setTheme('dark');
+    window.matchMedia("(prefers-color-scheme: dark)").addEventListener((e) => {
+      if (e.matches) {
+        setTheme("dark");
       } else {
-        setTheme('light');
+        setTheme("light");
       }
-    })
+    });
   }
 }
 
